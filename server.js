d1063ed7c151654818e04224fb90bc2ce974dc2c
Brown-scripts <nikkibrowm297@gmail.com>
2025-01-05 16:39:02 +0000

    added cors

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 const { errorHandler } = require('./middleware/errorHandler');
+const cors = require('cors');
+
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
